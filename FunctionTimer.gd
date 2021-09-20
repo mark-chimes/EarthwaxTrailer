6019bec10eba8f5e70fb5f9da4d9a1a7bce72bf9
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-09-20 15:18:31 +0200

    New function timing system

---
+++ FunctionTimer.gd
@@ -0,0 +1,43 @@
+extends Object
+
+class Timed_Function:
+	var active_time: float
+	var to_activate: FuncRef
+	
+var timed_functions = []
+var next_timed_function = null
+var timer = 0
+
+# TODO auto-sort if new active time is earlier than latest
+func add_function_at_time(active_time, func_to_activate): 
+	timed_functions.push_back(function_timer_for(active_time, func_to_activate))
+	
+func add_function_after(extra_time, func_to_activate):
+	if timed_functions.empty():
+		add_function_at_time(extra_time, func_to_activate)
+		return
+	var last_time = timed_functions.back().active_time
+	add_function_at_time(last_time + extra_time, func_to_activate)
+
+static func function_timer_for(active_time, to_activate): 
+	var timed_func = Timed_Function.new()
+	timed_func.active_time = active_time
+	timed_func.to_activate = to_activate
+	return timed_func
+
+func tick_process(delta): 
+	timer += delta
+	return perform_action_at_time(timer)
+
+func perform_action_at_time(timer): 
+	print("PERFORM ACTION AT TIME: " + str(timer))
+	if next_timed_function == null: 
+		if timed_functions.empty():
+			return false
+		next_timed_function = timed_functions.pop_front()
+	if timer >= next_timed_function.active_time: 
+		next_timed_function.to_activate.call_func()
+		if timed_functions.empty():
+			return false
+		next_timed_function = timed_functions.pop_front()
+	return true
--- TimingsController.gd
+++ TimingsController.gd
@@ -1,33 +1,42 @@
 extends Node2D
 
-var timer = 0
-
-var switch = 1
+var FunctionTimerScript = load("res://FunctionTimer.gd")
 
 signal shoot_first_arrow
 signal shoot_arrows
 signal people_enter
 var has_scene_started = false
+var timed_functions 
+var next_timed_function
+var has_ended = false
+
+var function_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	switch = 6
+	function_timer = FunctionTimerScript.new()
+	function_timer.add_function_at_time(6, funcref(self, 'shoot_first_arrow'))
+	function_timer.add_function_after(3, funcref(self, 'shoot_arrows'))
+	function_timer.add_function_after(2, funcref(self, 'people_enter'))
 	
 func start_scene(): 
 	has_scene_started = true
 	$Rabbits.start_scene()
 	$Rat.start_scene()
+
+func shoot_first_arrow(): 
+	emit_signal("shoot_first_arrow")
+
+func shoot_arrows(): 
+	emit_signal("shoot_arrows")
+	
+func people_enter(): 
+	emit_signal("people_enter")
 	
 func _process(delta):
-	if not has_scene_started:
+	if not has_scene_started or has_ended:
 		return
-	timer += delta
-	if timer >= 6 and switch == 6:
-		emit_signal("shoot_first_arrow")
-		switch = 9
-	if timer >= 9 and switch == 9:
-		emit_signal("shoot_arrows")
-		switch = 11
-	if timer >= 11 and switch == 11:
-		emit_signal("people_enter")
-		switch += 1
+		
+	var could_perform_action = function_timer.tick_process(delta)
+	has_ended = not could_perform_action
+
