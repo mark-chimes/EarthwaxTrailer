b11f9d9a77899210e6c69dcc417668341ea9d046
Mark Chimes <mark-chimes@users.noreply.github.com>
2022-02-28 19:53:08 +0200

    Move and delete a few items and objects.

--- Grass_parallax.gd
+++ Grass_parallax.gd
@@ -2,13 +2,3 @@ extends Node2D
 
 export var x_speed = 0.5
 
-func _process(delta):
-	$grassblock.position.x += 8*delta
-	$grassblock2.position.x += 18*delta
-	$grassblock3.position.x += 20*delta
-	$grassblock4.position.x += 22*delta
-	$grassblock5.position.x += 24*delta
-	$spekbushes.position.x += 18*delta
-	$spekbushes2.position.x += 20*delta
-	$spekbushes3.position.x += 22*delta
-	$spekbushes4.position.x += 24*delta
