00a7cbbacb0f4f3db8435c728bc164ce46fc46d0
Mark Chimes <mark-chimes@users.noreply.github.com>
2022-03-01 10:11:50 +0200

    Move everything into neat little folders.

---
+++ trailer/utils/timing/TimingsController.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var FunctionTimerScript = load("res://trailer/utils/timing/FunctionTimer.gd")
+
+signal shoot_first_arrow
+signal shoot_arrows
+signal people_enter
+
+var has_scene_started = false
+var has_ended = false
+
+var function_timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	function_timer = FunctionTimerScript.new()
+	function_timer.add_function_after(6, funcref(self, 'shoot_first_arrow'))
+	function_timer.add_function_after(3, funcref(self, 'shoot_arrows'))
+	function_timer.add_function_after(2, funcref(self, 'people_enter'))
+	
+func start_scene(): 
+	has_scene_started = true
+	$Rabbits.start_scene()
+	$Rat.start_scene()
+
+func shoot_first_arrow(): 
+	emit_signal("shoot_first_arrow")
+
+func shoot_arrows(): 
+	emit_signal("shoot_arrows")
+	
+func people_enter(): 
+	emit_signal("people_enter")
+	
+func _process(delta):
+	if not has_scene_started or has_ended:
+		return
+		
+	var could_perform_action = function_timer.tick_process(delta)
+	has_ended = not could_perform_action
+
