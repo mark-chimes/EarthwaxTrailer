ac51995554ee5bef62a38f77e3a560f86ba96d6e
Liam Chimes <stunbrick@gmail.com>
2022-03-08 16:05:29 +0200

    army fights in rows

--- desert_strike/ArmyGlut.gd
+++ desert_strike/ArmyGlut.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var Grubling = preload("res://desert_strike/creature/Grubling.tscn")
 onready var parallax_engine = get_parent().get_parent().get_node("ParallaxEngine")
-var grubling
+var grublings = []
 enum Dir {
 	LEFT = -1,
 	RIGHT = 1,
@@ -17,24 +17,31 @@ enum State {
 var state = State.IDLE
 
 func _ready():
-	add_grubling()
+	add_grubling(3)
+	add_grubling(7)
+	add_grubling(11)
+	add_grubling(15)
 	state = State.WALK
-	grubling.set_state(state, Dir.LEFT)
+	for grubling in grublings:
+		grubling.set_state(state, Dir.LEFT)
 
 func get_pos():
-	return grubling.real_pos.x
+	return grublings[0].real_pos.x
 
-func add_grubling():
-	grubling = Grubling.instance()
+func add_grubling(z_pos):
+	var grubling = Grubling.instance()
+	grublings.append(grubling)
 	add_child(grubling)
 	grubling.real_pos.x = 20
-	grubling.real_pos.z = 5
+	grubling.real_pos.z = z_pos
 	parallax_engine.add_object_to_parallax_world(grubling)
 
 func fight():
 	state = State.FIGHT
-	grubling.set_state(state, Dir.LEFT)
+	for grubling in grublings:
+		grubling.set_state(state, Dir.LEFT)
 
 func idle():
 	state = State.IDLE
-	grubling.set_state(state, Dir.LEFT)
+	for grubling in grublings:
+		grubling.set_state(state, Dir.LEFT)
--- desert_strike/ArmyHuman.gd
+++ desert_strike/ArmyHuman.gd
@@ -4,7 +4,7 @@ signal defeat
 
 var Farmer = preload("res://desert_strike/creature/Farmer.tscn")
 onready var parallax_engine = get_parent().get_parent().get_node("ParallaxEngine")
-var farmer
+var farmers = []
 enum Dir {
 	LEFT = -1,
 	RIGHT = 1,
@@ -20,27 +20,34 @@ enum State {
 var state = State.IDLE
 
 func _ready():
-	add_farmer()
+	add_farmer(3)
+	add_farmer(7)
+	add_farmer(11)
+	add_farmer(15)
 	state = State.WALK
-	farmer.set_state(state, Dir.RIGHT)
+	for farmer in farmers:
+		farmer.set_state(state, Dir.RIGHT)
 
 func get_pos():
-	return farmer.real_pos.x
+	return farmers[0].real_pos.x
 
-func add_farmer():
-	farmer = Farmer.instance()
+func add_farmer(z_pos):
+	var farmer = Farmer.instance()
+	farmers.append(farmer)
 	add_child(farmer)
 	farmer.real_pos.x = -20
-	farmer.real_pos.z = 5
+	farmer.real_pos.z = z_pos
 	parallax_engine.add_object_to_parallax_world(farmer)
 
 func fight():
 	state = State.FIGHT
-	farmer.set_state(state, Dir.RIGHT)
-	yield(get_tree().create_timer(2.0), "timeout")
+	for farmer in farmers:
+		farmer.set_state(state, Dir.RIGHT)
+	yield(get_tree().create_timer(5.0), "timeout")
 	emit_signal("defeat")
 	state = State.DIE
-	farmer.set_state(state, Dir.RIGHT)
+	for farmer in farmers:
+		farmer.set_state(state, Dir.RIGHT)
 
 func get_state():
 	return state
