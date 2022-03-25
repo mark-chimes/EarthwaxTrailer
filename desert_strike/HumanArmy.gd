0f7627f3c05133cf52b2f5cac9b1d3e4243705fd
Mark Chimes <mark-chimes@users.noreply.github.com>
2022-03-25 11:14:27 +0200

    Spawn archers behind farmers

--- desert_strike/HumanArmy.gd
+++ desert_strike/HumanArmy.gd
@@ -9,11 +9,12 @@ func _ready():
 	spawn_first_wave()
 
 func spawn_first_wave(): 
-	add_new_creatures(Archer, NUM_LANES * BANDS_SPAWNED)
 	add_new_creatures(Farmer, NUM_LANES * BANDS_SPAWNED)
-	
+	add_new_creatures(Archer, NUM_LANES * BANDS_SPAWNED)
+
 func spawn_new_wave(): 
-	add_new_creatures(Archer, 2)
 	add_new_creatures(Farmer, 2)
+	add_new_creatures(Archer, 2)
+
 
 
