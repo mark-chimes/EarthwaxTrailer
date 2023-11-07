7fd20a1c50e779cc010bbdd30362498c295dc272
mark_chimes <chimes.mark@gmail.com>
2023-11-07 01:45:20 +0100

    Battle test controller starts battle.

--- desert_strike/army/BattlefieldAdjudicator.gd
+++ desert_strike/army/BattlefieldAdjudicator.gd
@@ -1,3 +1,74 @@
 extends Node
 
+signal attacker_defeat
+signal defender_defeat
+
 # Oversees battles - alternative name: Battlefield / Adjudicator / Battlemaster
+
+var State = preload("res://desert_strike/State.gd")
+
+const BATTLE_SEP = 10
+const NUM_LANES = 4
+onready var rng = RandomNumberGenerator.new()
+
+var attacker = null
+var defender = null
+
+onready var is_initialized = false
+
+func start_battle_between_armies(attacking_warband, defending_warband): 
+	rng.randomize()
+	attacker = attacking_warband
+	defender = defending_warband
+	attacker.connect("defeat", self, "_attacker_defeat")
+	defender.connect("defeat", self, "_defender_defeat")
+	is_initialized = true
+
+func _process(delta):
+	if not is_initialized: 
+		return
+	continue_battle()
+
+func continue_battle():
+	if attacker.get_state() == State.Army.BATTLE or attacker.get_state() == State.Army.DIE\
+			or defender.get_state() == State.Army.BATTLE or defender.get_state() == State.Army.DIE:
+		return
+	
+	if abs(attacker.get_pos() - defender.get_pos()) < BATTLE_SEP:
+		var battlefront_base = (attacker.get_pos() + defender.get_pos())/2
+		var battlefronts = []
+		# TODO RANDOM
+		for i in range(0,NUM_LANES): 
+			var offset = rng.randf_range(-0.2, 0.2)
+			battlefronts.append(battlefront_base + offset)
+		
+		attacker.battle(battlefronts, defender.army_grid)
+		defender.battle(battlefronts, attacker.army_grid)
+		attacker.connect("front_line_ready", defender, "_on_front_line_ready")
+		defender.connect("front_line_ready", attacker, "_on_front_line_ready")
+		attacker.connect("creature_death", defender, "_on_enemy_creature_death")
+		defender.connect("creature_death", attacker, "_on_enemy_creature_death")
+		attacker.connect("attack", defender, "_on_get_attacked")
+		defender.connect("attack", attacker, "_on_get_attacked")
+		attacker.connect("projectile_attack", defender, "_on_enemy_projectile_attack")
+
+	
+func _attacker_defeat():
+	# TODO Defeat actions
+	disconnect_signals()
+	emit_signal("attacker_defeat")
+	
+func _defender_defeat():
+	# TODO Defeat actions
+	disconnect_signals()
+	emit_signal("defender_defeat")
+
+func disconnect_signals(): 
+	attacker.disconnect("front_line_ready", defender, "_on_front_line_ready")
+	defender.disconnect("front_line_ready", attacker, "_on_front_line_ready")
+	attacker.disconnect("creature_death", defender, "_on_enemy_creature_death")
+	defender.disconnect("creature_death", attacker, "_on_enemy_creature_death")
+	attacker.disconnect("attack", defender, "_on_get_attacked")
+	defender.disconnect("attack", attacker, "_on_get_attacked")
+	attacker.disconnect("projectile_attack", defender, "_on_enemy_projectile_attack")
+	defender.disconnect("projectile_attack", attacker, "_on_enemy_projectile_attack")
--- desert_strike/control/BattleTestController.gd
+++ desert_strike/control/BattleTestController.gd
@@ -1,69 +1,19 @@
 extends Node2D
 
 # TODO this is more of a battle controller than an entity controller at this point
-var State = preload("res://desert_strike/State.gd")
-
-const BATTLE_SEP = 10
-const NUM_LANES = 4
-onready var rng = RandomNumberGenerator.new()
-
-onready var parallax_engine = get_parent().get_node("ParallaxEngine")
 
 func _ready():
-	$HumanWarbandAttacking.connect("defeat", self, "_human_defeat")
-	$GlutWarbandAttacking.connect("defeat", self, "_glut_defeat")
+	$BattlefieldAdjudicator.connect("attacker_defeat", self, "_human_defeat")
+	$BattlefieldAdjudicator.connect("defender_defeat", self, "_glut_defeat")
 	
-	$HumanWarbandAttacking.set_army_start_offset(-30)
-	$GlutWarbandAttacking.set_army_start_offset(10)
+	$HumanWarbandAttacking.set_army_start_offset(-4)
+	$GlutWarbandAttacking.set_army_start_offset(4)
 	$HumanWarbandAttacking.start_army()
 	$GlutWarbandAttacking.start_army()
+	$BattlefieldAdjudicator.start_battle_between_armies($HumanWarbandAttacking, $GlutWarbandAttacking)
+
+func _human_defeat(): 
+	pass
 	
-	rng.randomize()
-
-func _process(delta):
-	battle_start()
-
-
-func battle_start():
-	if $HumanWarbandAttacking.get_state() == State.Army.BATTLE or $HumanWarbandAttacking.get_state() == State.Army.DIE\
-			or $GlutWarbandAttacking.get_state() == State.Army.BATTLE or $GlutWarbandAttacking.get_state() == State.Army.DIE:
-		return
-	
-	if abs($HumanWarbandAttacking.get_pos() - $GlutWarbandAttacking.get_pos()) < BATTLE_SEP:
-		var battlefront_base = ($HumanWarbandAttacking.get_pos() + $GlutWarbandAttacking.get_pos())/2
-		var battlefronts = []
-		# TODO RANDOM
-		for i in range(0,NUM_LANES): 
-			var offset = rng.randf_range(-0.2, 0.2)
-			battlefronts.append(battlefront_base + offset)
-		
-		$HumanWarbandAttacking.battle(battlefronts, $GlutWarbandAttacking.army_grid)
-		$GlutWarbandAttacking.battle(battlefronts, $HumanWarbandAttacking.army_grid)
-		$HumanWarbandAttacking.connect("front_line_ready", $GlutWarbandAttacking, "_on_front_line_ready")
-		$GlutWarbandAttacking.connect("front_line_ready", $HumanWarbandAttacking, "_on_front_line_ready")
-		$HumanWarbandAttacking.connect("creature_death", $GlutWarbandAttacking, "_on_enemy_creature_death")
-		$GlutWarbandAttacking.connect("creature_death", $HumanWarbandAttacking, "_on_enemy_creature_death")
-		$HumanWarbandAttacking.connect("attack", $GlutWarbandAttacking, "_on_get_attacked")
-		$GlutWarbandAttacking.connect("attack", $HumanWarbandAttacking, "_on_get_attacked")
-		$HumanWarbandAttacking.connect("projectile_attack", $GlutWarbandAttacking, "_on_enemy_projectile_attack")
-
-	
-func _human_defeat():
-	# TODO Defeat actions
-	disconnect_signals()
-	
-func _glut_defeat():
-	# TODO Defeat actions
-	disconnect_signals()
-
-func disconnect_signals(): 
-	$HumanWarbandAttacking.disconnect("front_line_ready", $GlutWarbandAttacking, "_on_front_line_ready")
-	$GlutWarbandAttacking.disconnect("front_line_ready", $HumanWarbandAttacking, "_on_front_line_ready")
-	$HumanWarbandAttacking.disconnect("creature_death", $GlutWarbandAttacking, "_on_enemy_creature_death")
-	$GlutWarbandAttacking.disconnect("creature_death", $HumanWarbandAttacking, "_on_enemy_creature_death")
-	$HumanWarbandAttacking.disconnect("attack", $GlutWarbandAttacking, "_on_get_attacked")
-	$GlutWarbandAttacking.disconnect("attack", $HumanWarbandAttacking, "_on_get_attacked")
-	$HumanWarbandAttacking.disconnect("projectile_attack", $GlutWarbandAttacking, "_on_enemy_projectile_attack")
-	$GlutWarbandAttacking.disconnect("projectile_attack", $HumanWarbandAttacking, "_on_enemy_projectile_attack")
-	$HumanWarbandAttacking.disconnect("many_deaths", self, "_on_many_human_deaths")
-	$GlutWarbandAttacking.disconnect("many_deaths", self, "_on_many_glut_deaths")
+func _glut_defeat(): 
+	pass
